9f7b51c1642dfac5efa632937bc760d3a769ecb2
Natalie Whitton <newhitton@gmail.com>
2020-11-09 15:50:22 +0100

    added CORS

--- src/controllers/tickets.js
+++ src/controllers/tickets.js
@@ -4,33 +4,33 @@ const { Ticket } = require('../models/ticket');
 const router = express.Router();
 
 // get all tickets
-router.get('/', async (req, res) => {
-  try {
-    const allTickets = await Ticket.find();
-    if (!allTickets) {
-      res.status(204).json({ message: 'No tickets found' });
-    } else {
-      return res.json(allTickets);
-    }
-  } catch (err) {
-    return res.status(500).send();
-  }
-});
+// router.get('/', async (req, res) => {
+//   try {
+//     const allTickets = await Ticket.find();
+//     if (!allTickets) {
+//       res.status(204).json({ message: 'No tickets found' });
+//     } else {
+//       return res.json(allTickets);
+//     }
+//   } catch (err) {
+//     return res.status(500).send();
+//   }
+// });
 
-// get one ticket
-router.get('/:ticketId', async (req, res) => {
-  try {
-    const aTicket = await Ticket.findOne({ _id: req.params.userId });
+// // get one ticket
+// router.get('/:ticketId', async (req, res) => {
+//   try {
+//     const aTicket = await Ticket.findOne({ _id: req.params.userId });
 
-    if (!aTicket) {
-      res.status(404).json({ message: 'user ID does not exist' });
-    } else {
-      return res.json(aTicket);
-    }
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+//     if (!aTicket) {
+//       res.status(404).json({ message: 'user ID does not exist' });
+//     } else {
+//       return res.json(aTicket);
+//     }
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
 
 // post a ticket
 router.post('/', async (req, res) => {
